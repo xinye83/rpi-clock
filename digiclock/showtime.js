70b7982e926167ad5b73604e0f3deb6023196206
Xin Ye <yexinwhu@gmail.com>
2021-12-17 16:14:12 -0600

    fix 12 hour clock

--- digiclock/showtime.js
+++ digiclock/showtime.js
@@ -3,15 +3,14 @@ function showTime() {
     var h = date.getHours() // 0 - 23
     var m = date.getMinutes() // 0 - 59
     var s = date.getSeconds() // 0 - 59
-    var session = 'AM'
+    var session = h < 12 ? 'AM' : 'PM'
+
+    if (h >= 12) {
+        h -= 12
+    }
 
     if (h == 0) {
         h = 12
-    }
-
-    if (h > 12) {
-        h = h - 12
-        session = 'PM'
     }
 
     h = h < 10 ? '0' + h : h
