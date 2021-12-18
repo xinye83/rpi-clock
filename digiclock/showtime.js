aebc917e51b672b96f6ab45ee947307a36a13a82
Xin Ye <yexinwhu@gmail.com>
2021-12-17 21:25:46 -0600

    24 hour clock and digital font

--- digiclock/showtime.js
+++ digiclock/showtime.js
@@ -3,21 +3,12 @@ function showTime() {
     var h = date.getHours() // 0 - 23
     var m = date.getMinutes() // 0 - 59
     var s = date.getSeconds() // 0 - 59
-    var session = h < 12 ? 'AM' : 'PM'
-
-    if (h >= 12) {
-        h -= 12
-    }
-
-    if (h == 0) {
-        h = 12
-    }
 
     h = h < 10 ? '0' + h : h
     m = m < 10 ? '0' + m : m
     s = s < 10 ? '0' + s : s
 
-    var time = h + ':' + m + ':' + s + ' ' + session
+    var time = h + ':' + m + ':' + s
     document.getElementById('digital-clock').innerText = time
     document.getElementById('digital-clock').textContent = time
 
