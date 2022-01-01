412a63c846ff349f20884c8fff5cb40556db2e90
Xin Ye <yexinwhu@gmail.com>
2022-01-01 16:00:47 -0600

    add a button

--- digiclock/digiclock.py
+++ digiclock/digiclock.py
@@ -9,8 +9,14 @@ app = Flask(__name__)
 
 
 @app.route("/", methods=["GET"])
+@app.route("/index", methods=["GET"])
 def index():
     return render_template("index.html")
+
+
+@app.route("/home", methods=["GET"])
+def home():
+    return render_template("home.html")
 
 
 targets = ["sun", "moon"]
